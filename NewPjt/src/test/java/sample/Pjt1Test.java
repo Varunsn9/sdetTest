28986544df65cc7baf7b673f4b896c2eacda56fa
Varunsn9 <varunsn9@gmail.com>
2023-09-05 17:44:30 +0530

    8

--- NewPjt/src/test/java/sample/Pjt1Test.java
+++ NewPjt/src/test/java/sample/Pjt1Test.java
@@ -2,16 +2,30 @@ package sample;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Pjt1Test {
+	WebDriver driver;
 	@Test
 	public void m1Test()
 	{
-		System.out.println("bye bye");
-		System.out.println("see you");
-	
+		String BROWSER = System.getProperty("browser");
+		 String URL = System.getProperty("url");
+	    if(BROWSER.equalsIgnoreCase("chrome"))
+	    {
+	    	WebDriverManager.chromedriver().setup();
+	    	driver=new ChromeDriver();
+	    }else if(BROWSER.equalsIgnoreCase("edge"))
+	    {
+	    	WebDriverManager.edgedriver().setup();
+	    	driver=new EdgeDriver();
+	    }else {
+	    	WebDriverManager.edgedriver().setup();
+	    	driver=new EdgeDriver();
+	    }
+	    driver.get(URL);
 	}
 }
